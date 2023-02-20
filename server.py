29ef47d64db115021ebd963495ba87ae0a19ec24
Hildaawada <125686901+Hildaawada@users.noreply.github.com>
2023-02-20 15:17:46 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from time import time
+from datetime import datetime
 import requests
 import socket
 #proxy server
@@ -25,17 +26,17 @@ while True:
         request_string=f"GET / HTTP/1.1\r\nHost:{INPUT}\r\n\r\n"
         # send the client's request to the destination server
         print("request to destination server:",request_string)
-        print("request to destination server at exact time",time())
+        print("request to destination server at exact time",datetime.utcnow())
         PSW.send(request_string.encode())
         response_string=PSW.recv(4096)
         # print a message that the response was recieved with the exact time
-        print("response received"," at exact time:",time())
+        print("response received"," at exact time:",datetime.utcnow())
         PSW.close()
         #print a message that the response was sent with the exact time
         c.send(response_string)
-        print("response sent"," at exact time:", time())
+        print("response sent"," at exact time:", datetime.utcnow())
         c.close()
         
-        print("total round trip time", time())
     except Exception as error:
         print("error:", error)
+
