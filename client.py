e7e5afdeba6186d69bedd73bb9c6b74d7e6f2694
Hildaawada <125686901+Hildaawada@users.noreply.github.com>
2023-02-20 15:21:03 +0200

    client.py

--- client.py
+++ client.py
@@ -1,20 +1,27 @@
 from time import time
 import uuid
 import socket
+from datetime import datetime
+start_time=time()
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 serverPort =4359
-s.connect(("192.168.0.106",serverPort))
+host_name=(socket.gethostbyname(socket.gethostname()))
+s.connect((host_name,serverPort))
 websiteIP = input('Enter desired IP address:')
 s.send(websiteIP.encode())
-modifiedSentence = s.recv(4096)
 #print a message with the request details and the exact time sent
-print("request received"," at exact time:",time())
+print("message sent at:", datetime.utcnow())
+print("request is:", websiteIP)
+modifiedSentence = s.recv(4096)
+# message received at time
 print ('From Server:')
-print(modifiedSentence.decode())
-print("exact time of response:", time())
+print(modifiedSentence)
+print("exact time of response:", datetime.utcnow())
 print ("The MAC address in formatted way is : ", end="")
 print (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
 for ele in range(0,8*6,8)][::-1]))
 s.close()
+end_time=time()
+totalroundtriptime=end_time-start_time
 # total round trip time, we get the time after w close all sockets and the response received
-print("total round trip time", time())
+print("total round trip time", totalroundtriptime)
